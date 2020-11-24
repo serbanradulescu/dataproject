23ad479ee6fcf0217374332b48fb2e69cb9a11e8
Your Name <madhav.jc@gmail.com>
2020-11-24 12:34:43 +0100

    second commit

--- TeamProject/dataproject-main/1/project.R
+++ TeamProject/dataproject-main/1/project.R
@@ -1,8 +1,7 @@
-##Packages
-install.packages("ggplot2")
 library(ggplot2)
 ##Importing the data from CSV files.
-WeightData <- data.frame(read.csv("C:\\Users\\madha\\Desktop\\Data Science\\TeamProject\\Weight.csv"))
-Survivaldata <- data.frame(read.csv("C:\\Users\\madha\\Desktop\\Data Science\\TeamProject\\Survival.csv"))
-Behaviourdata <- data.frame(read.csv("C:\\Users\\madha\\Desktop\\Data Science\\TeamProject\\behaviour.csv"))
+WeightData <- data.frame(read.csv("raw_data\\Weight.csv"))
+Survivaldata <- data.frame(read.csv("raw_data\\Survival.csv"))
+Behaviourdata <- data.frame(read.csv("raw_data\\behaviour.csv"))
 Behaviourdata[is.na(Behaviourdata)] = 0
+
