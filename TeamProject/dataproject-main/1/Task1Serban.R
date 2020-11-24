b2b8931b61b50a7858a938a097b55d781d49e927
spradulescu <serban-petre@mail.com>
2020-11-24 12:55:19 +0100

    Finally

---
+++ TeamProject/dataproject-main/1/Task1Serban.R
@@ -0,0 +1,2 @@
+#This is Serban's task, please do not interfere with this code unless 
+#you know you know better
--- TeamProject/dataproject-main/1/project.R
+++ TeamProject/dataproject-main/1/project.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 ##Importing the data from CSV files.
-weightData <- data.frame(read.csv("raw_data\\Weight.csv"))
-survivaldata <- data.frame(read.csv("raw_data\\Survival.csv"))
-behaviourdata <- data.frame(read.csv("raw_data\\behaviour.csv"))
-behaviourdata[is.na(behaviourdata)] = 0
+weight <- data.frame(read.csv("raw_data\\Weight.csv"))
+survival <- data.frame(read.csv("raw_data\\Survival.csv"))
+behaviour <- data.frame(read.csv("raw_data\\behaviour.csv"))
+behaviour[is.na(behaviour)] = 0
 
