60fe0666bc81e6c1da29daecaa22e716f113c152
Your Name <madhav.jc@gmail.com>
2020-11-24 12:44:27 +0100

    another comit that may work

--- TeamProject/dataproject-main/1/project.R
+++ TeamProject/dataproject-main/1/project.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 ##Importing the data from CSV files.
-WeightData <- data.frame(read.csv("raw_data\\Weight.csv"))
-Survivaldata <- data.frame(read.csv("raw_data\\Survival.csv"))
-Behaviourdata <- data.frame(read.csv("raw_data\\behaviour.csv"))
-Behaviourdata[is.na(Behaviourdata)] = 0
+weightData <- data.frame(read.csv("raw_data\\Weight.csv"))
+survivaldata <- data.frame(read.csv("raw_data\\Survival.csv"))
+behaviourdata <- data.frame(read.csv("raw_data\\behaviour.csv"))
+behaviourdata[is.na(behaviourdata)] = 0
 
