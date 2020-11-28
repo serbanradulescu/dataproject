52f9b7175e3f00b93949c39323fb23b3e37c4df3
Midelemi <mideolagab@gmail.com>
2020-11-28 23:17:02 +0100

    Start

--- TeamProject/dataproject-main/1/TaskGabriel1.R
+++ TeamProject/dataproject-main/1/TaskGabriel1.R
@@ -1,5 +1,17 @@
-### This Gabriel`s work
+### This Gabriel`s work, please, for our sake, do not alter without consent.
 
-source(importdata.R)
+source("importdata.R")
+
+## Analyze the effect of treatments on larvae weight
+
+### Hypothesis:
+
+#H0: Larval weight does not depend upon the concentration of insecticide it is exposed to. 
+
+#H1: Larval weight depends upon the concentration of insecticide it is exposed to. 
 
 
+### I am only using the histogram to check normality, and based on the histogram,
+### the weight does not follow a normal distribution.
+
+hist(weight$Weight..g.)
