d46aed8abfdbc79029f0f99d81f337c993c51c24
spradulescu <serban-petre@mail.com>
2020-12-01 13:52:07 +0100

    Serban commints 3

--- TeamProject/dataproject-main/1/Task1Serban.R
+++ TeamProject/dataproject-main/1/Task1Serban.R
@@ -32,28 +32,44 @@ ggplot(survival, aes(x=treatment, y=dead, fill=exp_round)) +
   geom_bar(stat="identity")
 
 #It is obvious that the treatment500 is different from the others.
-#It is hard to observe if there are differences between blocks, we procced to do a linear model with the blocks as median
+#It is hard to observe if there are differences between blocks, we proced to do a linear model with the blocks as median
 
 lm1 <- lm(dead ~ treatment + exp_round, data= survival)
 summary(lm1)
 
-
+#P value is bigger than 0.05 for: treatment500. The other treatments and the blocks show a p_value smaller than 0.5 so no significant effect.
+#We proceed to study the interaction betwenn treatments and blocks.
 
 lm1 <- lm(dead ~ treatment + exp_round +
             I(as.numeric(treatment) * as.numeric(exp_round)),
           data= survival)
 summary(lm1)
 
+#No interaction between treatments and blocks. (p_value = 0.3738)
+#We proceed with the fisher test, as the data is categorical.
+
 t1 <- table(survival$treatment, survival$dead)
 fisher.test(t1, simulate.p.value=TRUE)
 
+#A simple fisher test tells us that there are significant differences between treatments.
+#We proceed with a multi-comparation fisher test.
+
 fisher.multcomp(t1)
+
 #500 has significant differences with all of them, between them there is no significant difference
 
+#Part 2 - Analysis on time to pupate
 #ANOVA
+
+plot(y=survival$timetopup, x= survival$treatment)
+
+#We know from previous analysis that treatment 500 is different. We proceed to study the differences between blocks and treatments for the time to pupate
 
 lm1 <- lm(timetopup ~ treatment + exp_round, data= survival)
 summary(lm1)
+
+#P value is bigger than 0.05 for: treatment500. The other treatments and the blocks show a p_value smaller than 0.5 so no significant effect.
+#We proceed to study the interaction betwenn treatments and blocks.
 
 lm1 <- lm(timetopup ~ treatment + exp_round +
             I(as.numeric(treatment) * as.numeric(exp_round)),
@@ -65,7 +81,7 @@ summary(lm1)
 anv <- aov( lm(survival$timetopup ~ survival$treatment) )
 summary(anv)
 
-plot(y=survival$timetopup, x= survival$treatment)
+#Anova shows significant differences between treatments. We proceed with a HSD test.
 
 agri <- HSD.test(anv, 'survival$treatment', alpha = 0.05, group=TRUE, main = "HSD Test")
 plot(agri, main = "Observing statistical differences with agricola package", ylab = "Insects counted", xlab = "Type of spray", sub = "two groups of effects: a and b")
