e93a04bd00195c0ef3a643788fbf1fef85a57c97
Your Name <madhav.jc@gmail.com>
2020-12-01 14:01:32 +0100

    Some development in behaviour PCA

--- TeamProject/dataproject-main/1/Task1Madhav.R
+++ TeamProject/dataproject-main/1/Task1Madhav.R
@@ -1,9 +1,27 @@
 source("importdata.R")
 library(tidyr)
 library(dplyr)
+library(ggplot2)
+library(factoextra)
 ### Principal component analysis for the behaviour data
 ##First step: Converting the dataframe to numeric
 behaviour1 <- behaviour
 row.names(behaviour1) <- paste(behaviour1$sample, behaviour1$treatment,
                                behaviour1$ï..exp_round, sep = "_")
 b1 <- select(behaviour1, S:M)
+
+##Prcomping for Principal component analysis
+##a logical value indicating whether the variables should be scaled to 
+##have unit variance before the analysis takes place.
+##The default is FALSE for consistency with S, but in general scaling is advisable.
+pca1 <- prcomp(b1, scale = TRUE) 
+summary(pca1)
+##Plotting
+##Scree plot
+fviz_eig(pca1, addlabels = TRUE)
+##Individuals PCA
+fviz_pca_ind(pca1, col.ind = "cos2", 
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE)
+fviz_pca_ind(pca1)
+
