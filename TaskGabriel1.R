7eda2660ff9c9cd76b7dd09f9ba676e60ddbba51
Midelemi <mideolagab@gmail.com>
2020-12-13 20:26:56 +0100

    Improved

--- TaskGabriel1.R
+++ TaskGabriel1.R
@@ -6,6 +6,7 @@ library(car)
 library(agricolae)
 library(ggplot2)
 
+
 ## Analyze the effect of treatments on larvae weight(working with the dataframe weight)
 
 ### Hypothesis:
@@ -13,8 +14,8 @@ library(ggplot2)
 #H0: Larval weight does not depend upon the concentration of insecticide it is exposed to. 
 
 #H1: Larval weight depends upon the concentration of insecticide it is exposed to. 
-wgt <- weig
-lavalweight <- wgt
+
+lavalweight <- weig
 View(lavalweight)
 
 ### Make new data.frames from lavalweight for the initial and final time (1 & 4)
@@ -22,98 +23,63 @@ View(lavalweight)
 
 Initialwgt <- filter(lavalweight, Time == 1)
 Finalwgt <- filter(lavalweight, Time == 4)
-data.frame(Initialwgt)
-data.frame(Finalwgt)
+
+
 
 ###Find the difference between the final and initial weight. There is a difference
 ## in the number of row between the newly created data.frames, because some lavae
-#escaped before the final weight and some immediately after the fisrt weight, so 
+#escaped before the final weight and some immediately after the first weight, so 
 #there is a need to get those reps that have both the final and the initial weight 
 #to get the difference in weight.
 
+Fordifference <- filter(lavalweight, Time == 4)
+
 
 Initialweight <- replicate(153, 0)
-Finalwgt <- data.frame(Finalwgt, Initialweight)
+Fordifference <- data.frame(Fordifference, Initialweight)
 
-for(i in 1:nrow(Finalwgt)){
+for(i in 1:nrow(Fordifference)){
   for(j in 1:nrow(Initialwgt)){
-  if(Finalwgt$Rep[i] == Initialwgt$Rep[j]){
-    Finalwgt$Initialweight[i] <- Initialwgt$weight[j]
+  if(Fordifference$Rep[i] == Initialwgt$Rep[j]){
+    Fordifference$Initialweight[i] <- Initialwgt$weight[j]
   }}}
 
-colnames(Finalwgt)[colnames(Finalwgt) == "weight"] <- "Finalweight"
-Finalwgt$Difference <- (Finalwgt$Finalweight - Finalwgt$Initialweight)
-Finalwgt <-  select(Finalwgt, -Time)
+###Change the weight to final weight to differentiate between initial and final weight
+colnames(Fordifference)[colnames(Fordifference) == "weight"] <- "Finalweight"
 
-###Changing names of columns to differentiate initial and final time and weights
-colnames(Initialwgt)[colnames(Initialwgt) %in% c("Time", "weight")] <- 
-  c("InitialTime", "Initialweight")
+##Create another column for difference between final and initial weight
+Fordifference$Difference <- (Fordifference$Finalweight - Fordifference$Initialweight)
 
-colnames(Finalwgt)[colnames(Finalwgt) %in% c("Time", "weight")] <- 
-  c("FinalTime", "Finalweight")
+##Remove the column time from the data.frame
+Fordifference <-  select(Fordifference, -Time)
+
+##Arrange the column of the data.frame in the order below
+Fordifference <- Fordifference[c("Rep", "Treatment", "FIL", "Initialweight", 
+                                 "Finalweight", "Difference")]
 
 ##Check the structure of newly created dataframes
-str(Initialwgt)
-str(Finalwgt)
+str(Fordifference)
 
-## The treatment, FIL and Time were integer, so i needed to convert them to a factor for easier manipulation.
-Initialwgt$Treatment <- as.factor(Initialwgt$Treatment)
-Initialwgt$FIL <- as.factor(Initialwgt$FIL)
-Initialwgt$InitialTime <- as.factor(Initialwgt$InitialTime)
-str(Initialwgt)
+## The treatment, FIL and Time were integer, so i needed to convert them to a 
+##factor for easier manipulation.
+Fordifference$Treatment <- as.factor(Fordifference$Treatment)
+Fordifference$FIL <- as.factor(Fordifference$FIL)
 
-Finalwgt$Treatment <- as.factor(Finalwgt$Treatment)
-Finalwgt$FIL <- as.factor(Finalwgt$FIL)
-Finalwgt$FinalTime <- as.factor(Finalwgt$FinalTime)
-str(Finalwgt)
-
-###Checking normality with ggplot
-wgt %>%
-  ggplot(aes(x = Treatment, group = Treatment, y = weight)) +
-  geom_boxplot() + 
-  theme_classic() +
-  xlab("Treatment") + 
-  ylab("Weight")
-
-### it is obvious that Treatment500 is different from other treatments
-
-Initialwgt %>%
-  ggplot(aes(x = Treatment, group = Treatment, y = Initialweight)) +
-  geom_boxplot() + 
-  theme_classic() +
-  xlab("Treatment") + 
-  ylab("Weight")
+str(Fordifference)
 
 
-Finalwgt %>%
-  ggplot(aes(x = Treatment, group = Treatment, y = Finalweight)) +
-  geom_boxplot() + 
-  theme_classic() +
-  xlab("Treatment") + 
-  ylab("Weight")
-
-
-#One-way ANOVA to determine the effect of thiamethoxam on initial weight and Final weight
-IW <- aov(Initialweight ~ Treatment, data = Initialwgt)
-summary(IW)
-
-FW <- aov(Finalweight ~ Treatment, data = Finalwgt)
-summary(FW)
-
-###Based on the two ANOVA we do not have sufficient argument to accept the H1
-
-
+#One-way ANOVA to determine the effect of thiamethoxam on weight
+res.aov<- aov(Difference ~ Treatment, data = Fordifference)
+summary(res.aov)
 
 #Test for normality using the Shapiro–Wilk statistic.Making a Q-Q plot  
 ##This can only be done after ANOVA
 
-shapiro.test(IW$residuals)
-qqnorm(IW$residuals,  main = "Initial Weight")
-qqline(IW$residuals)
+shapiro.test(res.aov$residuals)
+qqnorm(res.aov$residuals)
+qqline(res.aov$residuals)
 
-shapiro.test(FW$residuals)
-qqnorm(FW$residuals,  main = "Final Weigt")
-qqline(FW$residuals)
+
 
 ##LeveneTest for equality of Variance
 #Hypothesis
@@ -121,30 +87,33 @@ qqline(FW$residuals)
 #H1: At least one variance is not equal
 
 
-leveneTest(Initialweight ~ Treatment, data = Initialwgt)
-leveneTest(Finalweight ~ Treatment, data = Finalwgt)
+leveneTest(Difference ~ Treatment, data = Fordifference)
+
 #Based on the LeveneTest, the variance for all the groups are equal
 
 
 #Kruskal Wallis to to determine the effect of thiamethoxam on pupal weight
 
-kruz <- kruskal(Initialwgt$Initialweight, trt = Initialwgt$Treatment)
+kruz <- kruskal(Fordifference$Difference, trt = Fordifference$Treatment)
 print(kruz)
 
-KR <- kruskal(Finalwgt$Finalweight, trt = Finalwgt$Treatment)
-KR
+##Make a plot for visual representation of the analysis
+Fordifference %>%
+  ggplot(aes(x = Treatment, group = Treatment, y = Difference, fill = Treatment)) +
+  geom_violin(trim = FALSE) + 
+  geom_boxplot(width = 0.2) +
+  scale_y_continuous(limits = c(0.15, 0.50)) +
+  theme_gray() +
+  xlab("Treatment") + 
+  ylab("Weight")
+  
 
-###Fisher's test
-Tfil <- table(wgt$FIL, wgt$Treatment)
-Tfil
-fisher.test(Tfil)
+ ###Fisher's test
+fil <- table(Fordifference$FIL, Fordifference$Treatment)
+fil
+fisher.test(fil)
 
-INFish <- table(Initialwgt$FIL, Initialwgt$Treatment)
-INFish
-fisher.test(INFish)
 
-FNFish <- table(Finalwgt$FIL, Finalwgt$Treatment)
-FNFish
-fisher.test(FNFish)
+
 ###########
 
