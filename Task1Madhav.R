529aea44dc985e03e4a41edb5a60abb5a5e8f473
Your Name <madhav.jc@gmail.com>
2021-01-15 19:16:23 +0100

    Very pretty MnM part

--- Task1Madhav.R
+++ Task1Madhav.R
@@ -55,10 +55,7 @@ fviz_eig(pca1, addlabels = TRUE)
 
 
 var <- get_pca_var(pca1)
-var
-head(var$coord)
-head(var$cos2)
-head(var$contrib)
+
 
 
 ##Heat map
@@ -74,48 +71,54 @@ autoplot(pca1, data = b2, colour = 'treatment', frame = TRUE, frame.type = 'norm
 
 ##Anovas for individual behaviours
 beh.aovS <- aov(S ~ treatment, data = b2)
-summary.aov(beh.aovS)
+
 beh.aovGR<- aov(GR ~ treatment, data = b2)
-summary.aov(beh.aovGR)
+
 beh.aovW <- aov(W ~ treatment, data = b2)
-summary.aov(beh.aovW)
+
 beh.aovF <- aov(F ~ treatment, data = b2)
-summary.aov(beh.aovF)
+
 beh.aovPR <- aov(PR ~ treatment, data = b2)
-summary.aov(beh.aovPR)
+
 beh.aovN <- aov(N ~ treatment, data = b2)
-summary.aov(beh.aovN)
+
 beh.aovM <- aov(M ~ treatment, data = b2)
-summary(beh.aovM)
+
 
 
 ##Shapiro Wilk test for normality for each behaviour
-shapiro.test(beh.aovS$residuals)
-shapiro.test(beh.aovGR$residuals)
-shapiro.test(beh.aovW$residuals)
-shapiro.test(beh.aovF$residuals)
-shapiro.test(beh.aovPR$residuals)
-shapiro.test(beh.aovN$residuals)
-shapiro.test(beh.aovM$residuals)
-
+SS <- shapiro.test(beh.aovS$residuals)
+print(SS[["p.value"]])
+SGR <- shapiro.test(beh.aovGR$residuals)
+print(SGR[["p.value"]])
+SW <- shapiro.test(beh.aovW$residuals)
+print(SW[["p.value"]])
+SF <- shapiro.test(beh.aovF$residuals)
+print(SF[["p.value"]])
+SPR <- shapiro.test(beh.aovPR$residuals)
+print(SPR[["p.value"]])
+SN <- shapiro.test(beh.aovN$residuals)
+print(SN[["p.value"]])
+SM <- shapiro.test(beh.aovM$residuals)
+print(SM[["p.value"]])
 ##From the p values of the Shapiro Wilk statistic we can conclude that 
 ##none of the behaviours follow a normal distribution. 
 
 ##Kruskal Wallis test
 krusS <- kruskal(b2$S, trt = b2$treatment, alpha = 0.05)
-print(krusS)
+print(krusS[["groups"]])
 krusGR <- kruskal(b2$GR, trt = b2$treatment, alpha = 0.05)
-print(krusGR)
+print(krusGR[["groups"]])
 krusW <- kruskal(b2$W, trt = b2$treatment, alpha = 0.05)
-print(krusW)
+print(krusW[["groups"]])
 krusF <- kruskal(b2$F, trt = b2$treatment, alpha = 0.05)
-print(krusF)
+print(krusF[["groups"]])
 krusPR <- kruskal(b2$PR, trt = b2$treatment, alpha = 0.05)
-print(krusPR)
+print(krusPR[["groups"]])
 krusN <- kruskal(b2$N, trt = b2$treatment, alpha = 0.05)
-print(krusN)
+print(krusN[["groups"]])
 krusM <- kruskal(b2$M, trt = b2$treatment, alpha = 0.05)
-print(krusM)
+print(krusM[["groups"]])
 
 ### Comparing the effect of weight on behaviour
 Weight1 <- filter(weig, FIL == 0, Time == 4)
@@ -123,10 +126,7 @@ AvgWeights <- as.numeric()
 for(i in levels(as.factor(Weight1$Treatment))){
    AvgWeights <-c(AvgWeights,mean(Weight1$weight[which(Weight1$Treatment == i)]))
 }
-AvgWeights
 
-M <- filter(Weight1, Treatment == 15, Time == 4)
-mean(M$weight)
 ##Calculating the average time spent by adults of each treatment group
 ##For each behavious
 
@@ -135,51 +135,53 @@ AvgS <- as.numeric()
 for(i in levels(as.factor(behaviour1$treatment))){
   AvgS <- c(AvgS, mean(behaviour1$S[which(behaviour1$treatment == i)]))
 }
-AvgS
+
 
 ##Avg GR
 AvgGR <- as.numeric()
 for(i in levels(as.factor(behaviour1$treatment))){
   AvgGR <- c(AvgGR, mean(behaviour1$GR[which(behaviour1$treatment == i)]))
 }
-AvgGR
+
 
 ##Avg W
 AvgW <- as.numeric()
 for(i in levels(as.factor(behaviour1$treatment))){
   AvgW <- c(AvgW, mean(behaviour1$W[which(behaviour1$treatment == i)]))
 }
-AvgW
+
 
 ##Avg F
 AvgF <- as.numeric()
 for(i in levels(as.factor(behaviour1$treatment))){
   AvgF <- c(AvgF, mean(behaviour1$F[which(behaviour1$treatment == i)]))
 }
-AvgF
+
 
 ##Avg PR
 AvgPR <- as.numeric()
 for(i in levels(as.factor(behaviour1$treatment))){
   AvgPR <- c(AvgPR, mean(behaviour1$PR[which(behaviour1$treatment == i)]))
 }
-AvgPR
+
 
 ##Avg N
 AvgN <- as.numeric()
 for(i in levels(as.factor(behaviour1$treatment))){
   AvgN <- c(AvgN, mean(behaviour1$N[which(behaviour1$treatment == i)]))
 }
-AvgN
+
 
 ##Avg M
 AvgM <- as.numeric()
 for(i in levels(as.factor(behaviour1$treatment))){
   AvgM <- c(AvgM, mean(behaviour1$M[which(behaviour1$treatment == i)]))
 }
-AvgM
+
+
 Treatments <- c(0, 5, 15, 50, 100, 500)
 WeiBeha <- data.frame(Treatments, AvgWeights, AvgS, AvgGR, AvgW, AvgF, AvgPR, AvgN, AvgM)
+
 
 WeiBeha2 <- WeiBeha %>%
   pivot_longer(!c(Treatments,AvgWeights), names_to = "Behaviour", values_to = "seconds")
