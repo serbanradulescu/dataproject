d2576457f2c74fb145e123a858de21474242b009
mayahachicha <hachichamaya@gmail.com>
2021-01-19 07:08:00 +0100

    explaining plot

--- Maya1.R
+++ Maya1.R
@@ -45,7 +45,7 @@ eig.val <- get_eigenvalue(pca1)
 ##Plotting
 ##Scree plot
 fviz_eig(pca1, addlabels = TRUE)
-
+#From the Eigen values and the Scree plot of our PCA we understand that Principal Component 1 and Principal Component 2 explain 46.4% of the variance in the data.
 
 ##From the Eigen Values and the Scree plot we that the first two principle 
 ## components explain 46.4% of the variation.
@@ -60,12 +60,19 @@ head(var$contrib)
 
 ##Heat map
 autoplot(var$cor)
+
+#From the heat map we can visualize that the first two dimensions explain a largest degree of variance in our data. The third and fourth compnent also explain a large degree of the variance. The fourth dimension is a good fit for the probing behaviour (PR). 
+
+
 ##Plotting the correlation circle
 fviz_pca_var(pca1, col.var = "black")
 
+# This plot of the correlation circle visualizes the correlation between the first two principal components which explain the highest variance which are respectively 27.6 % and 18.8%. We can conclude that Flying and walking are correlated together (the arrow are grouped), stationary and  grooming are correlated together and the arrow which reprents feeding on nectar, pollen, and water is in the opposite direction and not correlated with others behaviours.
 
 ##Individuals PCA
 autoplot(pca1, data = b2, colour = 'treatment', frame = TRUE, frame.type = 'norm')
+
+#This plot with ellipses shows the distribution of each behaviour following each treatment. From this plot we can conclude that there is no significant difference between the effect of treatment on
 
 #To determine the effect of treatments in behaviour we can apply Kruskal Wallis for each behaviour
 #to do this first we have to do anova then shapiro for residuals 
@@ -96,28 +103,158 @@ shapiro.test(anovN$residuals)
 shapiro.test(anovM$residuals)
 
 #Kruskal Wallis 
-krusS<-kruskal(beha$S, trt = beha$treatment, alpha= 0.01)
+krusS<-kruskal(beha$S, trt = beha$treatment, alpha= 0.05)
 
 print(krusS)
-krusGR<-kruskal(beha$GR, trt= beha$treatment, alpha = 0.01)
+krusGR<-kruskal(beha$GR, trt= beha$treatment, alpha = 0.05)
 print(krusGR)
-krusW<-kruskal(beha$W, trt = beha$treatment, alpha= 0.01)
+krusW<-kruskal(beha$W, trt = beha$treatment, alpha= 0.05)
 print(krusW)
-krusF<-kruskal(beha$F, trt = beha$treatment, alpha= 0.01)
+krusF<-kruskal(beha$F, trt = beha$treatment, alpha= 0.05)
 print(krusF)
-krusPR<-kruskal(beha$PR , trt =  beha$treatment, alpha= 0.01)
+krusPR<-kruskal(beha$PR , trt =  beha$treatment, alpha= 0.05)
 print(krusPR)
-krusN<-kruskal(beha$N, trt= beha$treatment, alpha= 0.01)
+krusN<-kruskal(beha$N, trt= beha$treatment, alpha= 0.05)
 print(krusN)
-krusM<-kruskal(beha$M , trt =  beha$treatment, alpha= 0.01)
+krusM<-kruskal(beha$M , trt =  beha$treatment, alpha= 0.05)
 print(krusM)
 
-#The effect of weight on behaviour 
-behawei<-weig
-Weight1 <- filter(weig, FIL == 0)
 
+### Comparing the effect of weight on behaviour
+Weight1 <- filter(weig, FIL == 0, Time == 4)
+AvgWeights <- as.numeric()
+for(i in levels(as.factor(Weight1$Treatment))){
+  AvgWeights <-c(AvgWeights,mean(Weight1$weight[which(Weight1$Treatment == i)]))
+}
+AvgWeights
 
+M <- filter(Weight1, Treatment == 15, Time == 4)
+mean(M$weight)
+##Calculating the average time spent by adults of each treatment group
+##For each behaviour
 
+##Avg S
+AvgS <- as.numeric()
+for(i in levels(as.factor(behaviour1$treatment))){
+  AvgS <- c(AvgS, mean(behaviour1$S[which(behaviour1$treatment == i)]))
+}
+AvgS
+
+##Avg GR
+AvgGR <- as.numeric()
+for(i in levels(as.factor(behaviour1$treatment))){
+  AvgGR <- c(AvgGR, mean(behaviour1$GR[which(behaviour1$treatment == i)]))
+}
+AvgGR
+
+##Avg W
+AvgW <- as.numeric()
+for(i in levels(as.factor(behaviour1$treatment))){
+  AvgW <- c(AvgW, mean(behaviour1$W[which(behaviour1$treatment == i)]))
+}
+AvgW
+
+##Avg F
+AvgF <- as.numeric()
+for(i in levels(as.factor(behaviour1$treatment))){
+  AvgF <- c(AvgF, mean(behaviour1$F[which(behaviour1$treatment == i)]))
+}
+AvgF
+
+##Avg PR
+AvgPR <- as.numeric()
+for(i in levels(as.factor(behaviour1$treatment))){
+  AvgPR <- c(AvgPR, mean(behaviour1$PR[which(behaviour1$treatment == i)]))
+}
+AvgPR
+
+##Avg N
+AvgN <- as.numeric()
+for(i in levels(as.factor(behaviour1$treatment))){
+  AvgN <- c(AvgN, mean(behaviour1$N[which(behaviour1$treatment == i)]))
+}
+AvgN
+
+##Avg M
+AvgM <- as.numeric()
+for(i in levels(as.factor(behaviour1$treatment))){
+  AvgM <- c(AvgM, mean(behaviour1$M[which(behaviour1$treatment == i)]))
+}
+AvgM
+Treatments <- c(0, 5, 15, 50, 100, 500)
+WeiBeha <- data.frame(Treatments, AvgWeights, AvgS, AvgGR, AvgW, AvgF, AvgPR, AvgN, AvgM)
+
+barplot(WeiBeha, height = 600, width = 7)
+### Comparing the effect of weight on behaviour
+Weight1 <- filter(weig, FIL == 0, Time == 4)
+AvgWeights <- as.numeric()
+for(i in levels(as.factor(Weight1$Treatment))){
+  AvgWeights <-c(AvgWeights,mean(Weight1$weight[which(Weight1$Treatment == i)]))
+}
+AvgWeights
+
+M <- filter(Weight1, Treatment == 15, Time == 4)
+mean(M$weight)
+##Calculating the average time spent by adults of each treatment group
+##For each behavious
+
+##Avg S
+AvgS <- as.numeric()
+for(i in levels(as.factor(behaviour1$treatment))){
+  AvgS <- c(AvgS, mean(behaviour1$S[which(behaviour1$treatment == i)]))
+}
+AvgS
+
+##Avg GR
+AvgGR <- as.numeric()
+for(i in levels(as.factor(behaviour1$treatment))){
+  AvgGR <- c(AvgGR, mean(behaviour1$GR[which(behaviour1$treatment == i)]))
+}
+AvgGR
+
+##Avg W
+AvgW <- as.numeric()
+for(i in levels(as.factor(behaviour1$treatment))){
+  AvgW <- c(AvgW, mean(behaviour1$W[which(behaviour1$treatment == i)]))
+}
+AvgW
+
+##Avg F
+AvgF <- as.numeric()
+for(i in levels(as.factor(behaviour1$treatment))){
+  AvgF <- c(AvgF, mean(behaviour1$F[which(behaviour1$treatment == i)]))
+}
+AvgF
+
+##Avg PR
+AvgPR <- as.numeric()
+for(i in levels(as.factor(behaviour1$treatment))){
+  AvgPR <- c(AvgPR, mean(behaviour1$PR[which(behaviour1$treatment == i)]))
+}
+AvgPR
+
+##Avg N
+AvgN <- as.numeric()
+for(i in levels(as.factor(behaviour1$treatment))){
+  AvgN <- c(AvgN, mean(behaviour1$N[which(behaviour1$treatment == i)]))
+}
+AvgN
+
+##Avg M
+AvgM <- as.numeric()
+for(i in levels(as.factor(behaviour1$treatment))){
+  AvgM <- c(AvgM, mean(behaviour1$M[which(behaviour1$treatment == i)]))
+}
+AvgM
+Treatments <- c(0, 5, 15, 50, 100, 500)
+WeiBeha <- data.frame(Treatments, AvgWeights, AvgS, AvgGR, AvgW, AvgF, AvgPR, AvgN, AvgM)
+
+barplot(WeiBeha, height = 500, width = 7)
+WB <- ggplot(data=WeiBeha, aes(x=AvgWeights, y=AvgS, AvgGR, AvgW, AvgF, AvgPR, AvgN, AvgM, fill=supp))
+
+geom_bar(stat = "identity")WB <- ggplot(data=WeiBeha, aes(x=AvgWeights, y=AvgS, AvgGR, AvgW, AvgF, AvgPR, AvgN, AvgM, fill=supp))
+
+geom_bar(stat = "identity")
 
 
 
